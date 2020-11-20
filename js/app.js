9c29a6e4ad4c6a2541b63a0c79cbb959d02c9909
AlvaroLuken <al5031@nyu.edu>
2020-11-19 17:28:07 -0800

    hey

--- js/app.js
+++ js/app.js
@@ -6,26 +6,6 @@
 var apiKey = "46995354";
 var sessionId = "2_MX40Njk5NTM1NH5-MTYwNTgyOTY1NjEwNH5Zb3JBcG1NY3A5NUl4ZGdwYXMwRG1WSFJ-fg";
 var token = "T1==cGFydG5lcl9pZD00Njk5NTM1NCZzaWc9MTdjNDA0MmU0MzRmODcyOGI3YTc3NjVjOTJlNDVmMzFlZDJhZjRiMzpzZXNzaW9uX2lkPTJfTVg0ME5qazVOVE0xTkg1LU1UWXdOVGd5T1RZMU5qRXdOSDVaYjNKQmNHMU5ZM0E1TlVsNFpHZHdZWE13UkcxV1NGSi1mZyZjcmVhdGVfdGltZT0xNjA1ODI5NjY2Jm5vbmNlPTAuNTAyMzk0NTQxMjg1Nzg2NiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjA1ODMzMjY1JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
-
-
-let tokenAddress = "0xB699d1B33CB82034BCdb6eB2a52bdd866f0744a4";
-let walletAddress;
-// Get ERC20 Token contract instance
-let contract = web3.eth.contract(minABI).at(tokenAddress);
-
-
-
-
-
-let currentAccount = null;
-ethereum.request({ method: 'eth_accounts' }).then((res) => {
-	console.log(res);
-	console.log(res[0]);
-	console.log(getBalance(res[0]));
-	
-});
-console.log(walletAddress);
-
 // The minimum ABI to get ERC20 Token balance
 let minABI = [
   // balanceOf
@@ -46,6 +26,26 @@ let minABI = [
   }
 ];
 
+let tokenAddress = "0xB699d1B33CB82034BCdb6eB2a52bdd866f0744a4";
+let walletAddress;
+// Get ERC20 Token contract instance
+let contract = web3.eth.contract(minABI).at(tokenAddress);
+
+
+
+
+
+let currentAccount = null;
+ethereum.request({ method: 'eth_accounts' }).then((res) => {
+	console.log(res);
+	console.log(res[0]);
+	console.log(getBalance(res[0]));
+	
+});
+console.log(walletAddress);
+
+
+
 
 // Call balanc
 
@@ -64,7 +64,7 @@ var SERVER_BASE_URL = 'https://second-try-remote.herokuapp.com';
       apiKey = res.apiKey;
       sessionId = res.sessionId;
       token = res.token;
-      if(getBalance() > 24) { //check if has COVD minimum!
+      if(true) { //check if has COVD minimum!
       	initializeSession();
       } else {
       	alert('gotta get mofe!');
