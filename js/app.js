4e00457e46ef0bdefb27b3d56ba96c5917b6503b
AlvaroLuken <al5031@nyu.edu>
2020-11-19 16:03:55 -0800

    more

--- js/app.js
+++ js/app.js
@@ -3,14 +3,17 @@ var apiKey = "46995354";
 var sessionId = "2_MX40Njk5NTM1NH5-MTYwNTgyOTY1NjEwNH5Zb3JBcG1NY3A5NUl4ZGdwYXMwRG1WSFJ-fg";
 var token = "T1==cGFydG5lcl9pZD00Njk5NTM1NCZzaWc9MTdjNDA0MmU0MzRmODcyOGI3YTc3NjVjOTJlNDVmMzFlZDJhZjRiMzpzZXNzaW9uX2lkPTJfTVg0ME5qazVOVE0xTkg1LU1UWXdOVGd5T1RZMU5qRXdOSDVaYjNKQmNHMU5ZM0E1TlVsNFpHZHdZWE13UkcxV1NGSi1mZyZjcmVhdGVfdGltZT0xNjA1ODI5NjY2Jm5vbmNlPTAuNTAyMzk0NTQxMjg1Nzg2NiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjA1ODMzMjY1JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
 
+const Web3 = require("web3");
+const ethers = require('ethers');
+var web3 = new Web3(Web3.givenProvider || 'http://localhost:3000');
 
 
-// Handling all of our errors here by alerting them
-function handleError(error) {
-  if (error) {
-    alert(error.message);
-  }
+
+if(ethereum.isConnected()) {
+	alert('heyyy');
 }
+
+
 
 var SERVER_BASE_URL = 'https://second-try-remote.herokuapp.com';
     fetch(SERVER_BASE_URL + '/session').then(function(res) {
@@ -34,6 +37,13 @@ function initializeSession() {
 	    height: '100%'
 	  }, handleError);
   });
+
+  // Handling all of our errors here by alerting them
+function handleError(error) {
+  if (error) {
+    alert(error.message);
+  }
+}
 
   // Create a publisher
   var publisher = OT.initPublisher('publisher', {
