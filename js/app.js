83acfa86f7ff24b51705f939dff376a9007818d8
AlvaroLuken <al5031@nyu.edu>
2020-11-19 16:06:39 -0800

    nbow

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
+const Web3 = require("web3");
+const ethers = require('ethers');
+var web3 = new Web3(Web3.givenProvider || 'http://localhost:3000');
+
 // replace these values with those generated in your TokBox Account
 var apiKey = "46995354";
 var sessionId = "2_MX40Njk5NTM1NH5-MTYwNTgyOTY1NjEwNH5Zb3JBcG1NY3A5NUl4ZGdwYXMwRG1WSFJ-fg";
 var token = "T1==cGFydG5lcl9pZD00Njk5NTM1NCZzaWc9MTdjNDA0MmU0MzRmODcyOGI3YTc3NjVjOTJlNDVmMzFlZDJhZjRiMzpzZXNzaW9uX2lkPTJfTVg0ME5qazVOVE0xTkg1LU1UWXdOVGd5T1RZMU5qRXdOSDVaYjNKQmNHMU5ZM0E1TlVsNFpHZHdZWE13UkcxV1NGSi1mZyZjcmVhdGVfdGltZT0xNjA1ODI5NjY2Jm5vbmNlPTAuNTAyMzk0NTQxMjg1Nzg2NiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjA1ODMzMjY1JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
 
-const Web3 = require("web3");
-const ethers = require('ethers');
-var web3 = new Web3(Web3.givenProvider || 'http://localhost:3000');
+
 
 console.log("1");
 
