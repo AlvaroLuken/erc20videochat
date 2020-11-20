fd68e52e38b67f6c3edb47f230d3b91503d6ecf1
AlvaroLuken <al5031@nyu.edu>
2020-11-19 16:35:33 -0800

    hey

--- js/app.js
+++ js/app.js
@@ -8,9 +8,13 @@ var sessionId = "2_MX40Njk5NTM1NH5-MTYwNTgyOTY1NjEwNH5Zb3JBcG1NY3A5NUl4ZGdwYXMwR
 var token = "T1==cGFydG5lcl9pZD00Njk5NTM1NCZzaWc9MTdjNDA0MmU0MzRmODcyOGI3YTc3NjVjOTJlNDVmMzFlZDJhZjRiMzpzZXNzaW9uX2lkPTJfTVg0ME5qazVOVE0xTkg1LU1UWXdOVGd5T1RZMU5qRXdOSDVaYjNKQmNHMU5ZM0E1TlVsNFpHZHdZWE13UkcxV1NGSi1mZyZjcmVhdGVfdGltZT0xNjA1ODI5NjY2Jm5vbmNlPTAuNTAyMzk0NTQxMjg1Nzg2NiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjA1ODMzMjY1JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
 
 
+let id;
 
+ethereum.enable().then(() => {
+  id = ethereum.selectedAddress
+}
 console.log("1");
-const id = ethereum.selectedAddress;
+
 console.log("Id is: " + id);
 
 
