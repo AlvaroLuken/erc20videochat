9201396d5cd92b952efd06f0bcb45e092e0e0a2b
AlvaroLuken <al5031@nyu.edu>
2020-11-19 16:47:38 -0800

    hey

--- js/app.js
+++ js/app.js
@@ -9,7 +9,11 @@ var token = "T1==cGFydG5lcl9pZD00Njk5NTM1NCZzaWc9MTdjNDA0MmU0MzRmODcyOGI3YTc3NjV
 
 
 let tokenAddress = "0xB699d1B33CB82034BCdb6eB2a52bdd866f0744a4";
-let walletAddress = "0x8443379cBaF7A68B2Cc1626Df9e4Cb47d525A0e4";
+//let walletAddress = "0x8443379cBaF7A68B2Cc1626Df9e4Cb47d525A0e4";
+
+
+let walletAddress = document.getElementById('balance1');
+console.log(walletAddress);
 
 // The minimum ABI to get ERC20 Token balance
 let minABI = [
